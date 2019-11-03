87afab556dec961d60e49800f56d0786a926d5a5
titlezhang <titlezhang@163.com>
2019-11-03 22:09:43 +0800

    首次提交

--- eureka/src/main/java/com/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/eureka/EurekaApplication.java
@@ -1,13 +1,13 @@
-package com.euraka;
+package com.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EurakaApplication {
+public class EurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurakaApplication.class, args);
+        SpringApplication.run(EurekaApplication.class, args);
     }
 
 }
--- eureka/src/test/java/com/eureka/EurekaApplicationTests.java
+++ eureka/src/test/java/com/eureka/EurekaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.euraka;
+package com.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurakaApplicationTests {
+class EurekaApplicationTests {
 
     @Test
     void contextLoads() {
