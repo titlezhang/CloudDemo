2d341b06b8c49ee018823598897486c378a62237
titlezhang <titlezhang@163.com>
2019-11-03 21:30:45 +0800

    首次提交

---
+++ euraka/src/main/java/com/euraka/EurakaApplication.java
@@ -0,0 +1,13 @@
+package com.euraka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurakaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurakaApplication.class, args);
+    }
+
+}
