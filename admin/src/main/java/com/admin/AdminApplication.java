b991e99b23aefa7a3040c8ad09be41d4dd921bb5
titlezhang <titlezhang@163.com>
2020-07-14 18:55:33 +0800

    crawler

--- admin/src/main/java/com/admin/AdminApplication.java
+++ admin/src/main/java/com/admin/AdminApplication.java
@@ -1,9 +1,20 @@
 package com.admin;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import jdk.nashorn.internal.parser.JSONParser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,5 +23,4 @@ public class AdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
     }
-
 }
